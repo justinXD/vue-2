95983e067152733065a14aaa182a59a86e3c01b7
Justin D <isaac_justin@hotmail.com>
2022-10-04 16:36:23 -0500

    algo sencillo con vue 2

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            name: 'Bitcoin',
+            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            changePercent: 0,
+            prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200]
+        }
+    },
+})
